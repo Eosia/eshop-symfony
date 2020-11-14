0bdae65730f938d2b191fc0fded6f3cb010d1fb5
unknown <kevin.eosiaweb@gmail.com>
2020-11-14 16:50:12 +0100

    creation barre de recherche avec filtres

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -22,13 +25,23 @@ class ProductController extends AbstractController
     /**
      * @Route("/nos-produits", name="products")
      */
-    public function index(): Response
+    public function index(Request  $request): Response
     {
         
         $products = $this->entityManager->getRepository(Product::class)->findAll();
         
+        $search = new Search();
+        $form = $this->createForm(SearchType::class, $search);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $products = $this->entityManager->getRepository(Product::class)->findWithSearch($search);
+        }
+
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
     
@@ -46,8 +59,8 @@ class ProductController extends AbstractController
             return $this->redirectToRoute('products');
         }
         
-        return $this->render('product/index.html.twig', [
-            'products' => $products
+        return $this->render('product/show.html.twig', [
+            'product' => $product
         ]);
     }
     
